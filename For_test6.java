1d8df0f60ac1d03652a0c03003092b10579e83ee
hongJunPark <gimy5240@gmail.com>
2024-05-23 22:29:24 +0900

    test

---
+++ For_test6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class For_test6 {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ch = sc.next().charAt(1);
+		
+		for(int i = 65; i <= 10; i++) {
+			
+			
+		}
+		System.out.printf(" = %d", j);
+		
+	}
+}
